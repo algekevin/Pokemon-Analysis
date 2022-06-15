acd252891489a353c09defc290dc54fb3ca3bdba
Kevin Bailey <algekevin@gmail.com>
2022-06-15 15:08:58 -0700

    added a simple boxplot and modified one column to be a character.

--- pokeList.R
+++ pokeList.R
@@ -1,6 +1,22 @@
-library(openxlsx)
-library(dplyr)
-library(ggplot2)
+library(dplyr) # piping and such
+library(tidyr) # replace_na
+library(ggplot2) 
+library(GLDEX) # which.na
 
-data <- read.csv("Pokemon.csv")
-View(data)
+data <- read.csv("Pokemon.csv", na="None")
+data <- data[,-1]
+data[data==""] <- "None" # Filling in the blanks
+
+# Generation is numeric, change here to be character to plot
+data$Generation = as.character(data$Generation)
+
+mean(data[data$Generation == 1 & data$Evolution == "Final", ]$Attack)
+
+ggplot(data, aes(x = Generation, 
+                 y = Total, 
+                 fill = Generation)) + 
+  geom_boxplot(outlier.color="red", 
+               outlier.shape=21, 
+               outlier.fill="red",
+               outlier.size=4) 
+
